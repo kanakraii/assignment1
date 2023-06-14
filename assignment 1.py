53add3364e18a8bdabe4ded6c84abf19de138eed
kanakraii <136440166+kanakraii@users.noreply.github.com>
2023-06-14 18:25:21 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,10 @@
+print("*Enter student details*")
+name=input("Enter name of the student:")
+n1=int(input("Enter marks for subject 1:"))
+n2=int(input("Enter marks for subject 2:"))
+n3=int(input("Enter marks for subject 3:"))
+print("Name of the student:",name)
+print("Marks of three subs:",n1,n2,n3)
+avg=(n1+n2+n3)/3
+print("The average of n1,n2 and n3 is:",avg)
+
